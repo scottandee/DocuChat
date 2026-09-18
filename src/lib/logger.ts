02ba322e46c0247f5d2085e42224f1a00d799d47
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2026-09-18 14:24:48 +0100

    feat: set-up logger with pino

---
+++ src/lib/logger.ts
@@ -0,0 +1,17 @@
+import pino from "pino";
+import { config } from "./config.ts";
+
+export const logger = pino({
+    level: config.NODE_ENV === "production" ? "info": "debug",
+    transport: 
+        config.NODE_ENV !== "production" 
+            ?   {
+                    target: "pino-pretty",
+                    options: {
+                        colorize: "true",
+                        translateTime: "SYS:standard",
+                        ignore: "pid,hostname",
+                    }
+                }
+            : undefined,
+})
