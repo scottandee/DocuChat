81feb422c0ca86e11a8141b2d493bf0f9d09dca8
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2026-09-19 16:34:08 +0100

    feat: create user repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,41 @@
+import { prisma } from "../lib/prisma.ts"
+
+export const userRepository = {
+    async findById(id: string) {
+        return await prisma.user.findUnique({
+            where: { id },
+        });
+    },
+
+    async findByEmail(email: string) {
+        return await prisma.user.findUnique({
+            where: { email }
+        });
+    },
+
+    async create(data: {
+        name: string,
+        email: string,
+        passwordHash: string,
+    }) {
+        return prisma.user.create({ data });
+    },
+
+    async updateById(id: string, data: {
+        name: string,
+        email: string,
+        isActive: boolean,
+    }) {
+        return prisma.user.update({
+            where: { id },
+            data
+        });
+    },
+
+    async softDelete(id: string) {
+        return prisma.user.update({
+            where: { id },
+            data: { deletedAt: new Date() },
+        });
+    },
+}
