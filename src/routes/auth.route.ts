455a5cc26fff84040d579786db806fef1b7f5739
Andee Olayinkascott <andeeolayinkascott@gmail.com>
2026-09-21 02:55:21 +0100

    feat: add validation to auth routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,12 +5,14 @@ import {
     refreshTokenController,
     registerController
 } from "../controllers/auth.controller.ts";
+import { validate } from "../middlewares/validate.middleware.ts";
+import { loginSchema, refreshSchema, registerSchema } from "../validators/auth.validator.ts";
 
 const router = Router();
 
-router.post("/register", registerController);
-router.post("/login", loginController);
-router.post("/refresh", refreshTokenController);
-router.post("/logout", logoutController);
+router.post("/register", validate(registerSchema), registerController);
+router.post("/login", validate(loginSchema), loginController);
+router.post("/refresh", validate(refreshSchema), refreshTokenController);
+router.post("/logout", validate(refreshSchema), logoutController);
 
 export default router;
---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,25 @@
+import z, { email } from "zod";
+
+export const registerSchema = z.object({
+    body: z.object({
+        email: z.email("Must be a valid email address"),
+        password: z.string()
+            .min(8, "Password must be at least 8 characters")
+            .max(128, "Password must not exceed 128 characters")
+            .regex(/[A-Z]/, "Must contain an uppercase letter")
+            .regex(/[0-9]/, "Must contain a number")
+    }),
+});
+
+export const loginSchema = z.object({
+    body: z.object({
+        email: z.email("Must provide valid email"),
+        password: z.string().min(1, "Password is required"),
+    }),
+});
+
+export const refreshSchema = z.object({
+    body: z.object({
+        refreshToken: z.string().min(1, "Refresh token is required"),
+    }),
+});
